6e9f5ee21af465f0bf333c9a0a89abdf094acc9c
Xiaoyu Huang <xiaoyu.huang@btcchina.com>
2019-04-03 08:17:27 +0900

    Remove test.db after test

--- resource/datastore_test.go
+++ resource/datastore_test.go
@@ -13,6 +13,7 @@ func TestMain(m *testing.M) {
 	// Ensure testdata dir exists
 	_ = os.MkdirAll(testdataDir, os.ModePerm)
 	retCode := m.Run()
+	_ = os.RemoveAll(testdataDir)
 	os.Exit(retCode)
 }
 
